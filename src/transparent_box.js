abd986ea9a7752491ab52f40d210bc1552edf434
Steven Senkowski <scsenkowski@gmail.com>
2018-10-17 08:04:14 -0400

    added React pose object

---
+++ src/transparent_box.js
@@ -0,0 +1,25 @@
+import ReactDOM from 'react-dom';
+import posed from 'react-pose';
+import './styles.css';
+
+const Box = posed.div({
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 }
+});
+
+class Example extends React.Component {
+  state = { isVisible: true };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ isVisible: !this.state.isVisible });
+    }, 1000);
+  }
+
+  render() {
+    const { isVisible } = this.state;
+    return <Box className="box" pose={isVisible ? 'visible' : 'hidden'} />;
+  }
+}
+
+ReactDOM.render(<Example />, document.getElementById('root'));
